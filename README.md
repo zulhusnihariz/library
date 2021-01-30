c1e1e434ac3f08e299db19e4412ea9db28673c04
zulhusnihariz <zulhusni_97@yahoo.com>
2021-01-30 21:13:14 +0800

    added on hover effect for display; amend few variables in js from const to let

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 The Odin Project - Full Stack Javascript - First Project: Library
+
+Learning Outcome:
+
+1. Retrieve input from input field and save into an Array in JS.
+2. Transform :hover effect
--- js/main.js
+++ js/main.js
@@ -1,7 +1,4 @@
-const bookTitle = document.getElementById("book-title").value;
-const bookAuthor = document.getElementById("book-author").value;
-const bookPages = document.getElementById("book-pages").value;
-const bookRead = document.querySelectorAll('input[name = "readStatus"]');
+let bookRead = document.querySelectorAll('input[name = "readStatus"]');
 const displayContainer = document.querySelector(".display-container");
 
 const addButton = document.getElementById("add-book");
@@ -21,25 +18,50 @@ Book.prototype.addBookToLibrary = function (bookName) {
 };
 
 function displayBooks() {
+	let booksCard;
+	let authorPages;
 	for (const books in myLibrary) {
 		d = document.createElement("div");
 		displayContainer.appendChild(d);
 		d.className = "booksCard";
-	}
-
-	let booksCard = document.querySelectorAll(".booksCard");
-
-	for (const books in myLibrary) {
+		booksCard = document.querySelectorAll(".booksCard");
 		booksCard[books].style.border = "3px solid black";
+		booksCard[books].style.textTransform = "uppercase";
 		booksCard[books].style.backgroundColor = "#22223b";
-
 		booksCard[books].textContent = myLibrary[books]["title"];
+
+		d2 = document.createElement("div");
+		booksCard[books].appendChild(d2);
+		d2.className = "authorPages";
+		authorPages = document.querySelectorAll(".authorPages");
+		authorPages[books].style.marginTop = "10px";
+		authorPages[books].style.lineHeight = "2.6";
+		authorPages[books].style.textTransform = "capitalize";
+		authorPages[books].style.color = "#22223b";
+		authorPages[books].innerHTML +=
+			myLibrary[books]["author"] +
+			"<br>" +
+			myLibrary[books]["pages"] +
+			" pages";
 	}
+}
+
+function clearField() {
+	document.getElementById("book-title").value = "";
+	document.getElementById("book-author").value = "";
+	document.getElementById("book-pages").value = "";
 }
 
 addButton.addEventListener("click", e => {
 	let newBook;
 	let readStatus;
+	let bookTitle;
+	let bookAuthor;
+	let bookPages;
+
+	bookTitle = document.getElementById("book-title").value;
+	bookAuthor = document.getElementById("book-author").value;
+	bookPages = document.getElementById("book-pages").value;
 
 	for (const option of bookRead) {
 		if (option.checked) {
@@ -50,10 +72,16 @@ addButton.addEventListener("click", e => {
 
 	newBook = new Book(bookTitle, bookAuthor, bookPages, readStatus);
 
+	console.log(bookTitle);
+	console.log(bookAuthor);
+	console.log(bookPages);
+	console.log(readStatus);
+
 	newBook.addBookToLibrary(newBook);
 	displayBooks();
-
 	e.preventDefault();
+	clearField();
+	console.log(myLibrary);
 });
 
 const book1 = new Book(
@@ -74,13 +102,20 @@ const book4 = new Book(
 	"not read yet"
 );
 
-const book5 = new Book("The Republic ", "Plato", "416", "not read yet");
+const book5 = new Book("The Republic", "Plato", "416", "not read yet");
+const book6 = new Book(
+	"Dunia Tanpa Tembok ",
+	"Ayman Rashdan Wong",
+	"245",
+	"not read yet"
+);
 
 book1.addBookToLibrary(book1);
 book2.addBookToLibrary(book2);
 book3.addBookToLibrary(book3);
 book4.addBookToLibrary(book4);
 book5.addBookToLibrary(book5);
+book6.addBookToLibrary(book6);
 
 window.onload = displayBooks;
 
