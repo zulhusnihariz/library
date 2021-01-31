33bc81b3df271c44b3892cb896c7166abb00dc00
zulhusnihariz <zulhusni_97@yahoo.com>
2021-01-31 18:02:41 +0800

    reduce displayBooks function into few functions; created new function for addButton on click to display new book

--- js/main.js
+++ js/main.js
@@ -3,8 +3,10 @@ const displayContainer = document.querySelector(".display-container");
 
 const addButton = document.getElementById("add-book");
 
+// Empty array to store information about books
 let myLibrary = [];
 
+// Create Book object
 function Book(title, author, pages, readStatus) {
 	this.title = title;
 	this.author = author;
@@ -12,46 +14,106 @@ function Book(title, author, pages, readStatus) {
 	this.readStatus = readStatus;
 	// the constructor
 }
-
+// set function addBooktoLibrary to Book Object
 Book.prototype.addBookToLibrary = function (bookName) {
 	myLibrary.push(bookName);
 };
 
+// create a card for each book in myLibrary and display on screen
 function displayBooks() {
 	let booksCard;
-	let authorPages;
+
 	for (const books in myLibrary) {
+		// loop and create div for every books that exist in Mylibrary
 		d = document.createElement("div");
 		displayContainer.appendChild(d);
 		d.className = "booksCard";
 		booksCard = document.querySelectorAll(".booksCard");
+
+		// styling each div for title
 		booksCard[books].style.border = "3px solid black";
 		booksCard[books].style.textTransform = "uppercase";
 		booksCard[books].style.backgroundColor = "#22223b";
 		booksCard[books].textContent = myLibrary[books]["title"];
+	}
+	// call function to create button
+	showAuthorPages(booksCard);
+	createButton(booksCard);
+}
 
+// display new card when add button is clicked
+function showNewBook() {
+	d = document.createElement("div");
+	displayContainer.appendChild(d);
+	d.className = "booksCard";
+	booksCard = document.querySelectorAll(".booksCard");
+
+	for (const books in myLibrary) {
+		// styling each div for title
+		booksCard[books].style.border = "3px solid black";
+		booksCard[books].style.textTransform = "uppercase";
+		booksCard[books].style.backgroundColor = "#22223b";
+		booksCard[books].textContent = myLibrary[books]["title"];
+	}
+
+	showAuthorPages(booksCard);
+	createButton(booksCard);
+}
+
+function showAuthorPages(booksCard) {
+	let authorPages;
+	// create another div within booksCard to display author & pages
+	for (const books in myLibrary) {
 		d2 = document.createElement("div");
 		booksCard[books].appendChild(d2);
 		d2.className = "authorPages";
 		authorPages = document.querySelectorAll(".authorPages");
+		// styling each div for author and pages
 		authorPages[books].style.marginTop = "10px";
 		authorPages[books].style.lineHeight = "2.6";
 		authorPages[books].style.textTransform = "capitalize";
 		authorPages[books].style.color = "#22223b";
+		authorPages[books].style.display = "none";
+
+		// add author and pages with break
 		authorPages[books].innerHTML +=
 			myLibrary[books]["author"] +
 			"<br>" +
 			myLibrary[books]["pages"] +
-			" pages";
+			" pages" +
+			"<br>" +
+			myLibrary[books]["readStatus"];
 	}
 }
 
+//create update and delete button
+function createButton(booksCard) {
+	for (const books in myLibrary) {
+		btn = document.createElement("BUTTON"); // Create a <button> element
+		booksCard[books].appendChild(btn);
+		btn.innerHTML = "Update"; // Insert text
+		btn.id = "hiddenButton";
+
+		booksCard[books].addEventListener("mouseover", e => {
+			e.target.querySelector("#hiddenButton").style.display = "inline";
+			e.target.querySelector(".authorPages").style.display = "block";
+		});
+
+		booksCard[books].addEventListener("mouseleave", e => {
+			e.target.querySelector("#hiddenButton").style.display = "none";
+			e.target.querySelector(".authorPages").style.display = "none";
+		});
+	}
+}
+
+// clear input field once added to myLibrary
 function clearField() {
 	document.getElementById("book-title").value = "";
 	document.getElementById("book-author").value = "";
 	document.getElementById("book-pages").value = "";
 }
 
+// onclick event for add book button
 addButton.addEventListener("click", e => {
 	let newBook;
 	let readStatus;
@@ -72,42 +134,43 @@ addButton.addEventListener("click", e => {
 
 	newBook = new Book(bookTitle, bookAuthor, bookPages, readStatus);
 
-	console.log(bookTitle);
-	console.log(bookAuthor);
-	console.log(bookPages);
-	console.log(readStatus);
-
 	newBook.addBookToLibrary(newBook);
-	displayBooks();
+	showNewBook();
 	e.preventDefault();
 	clearField();
 	console.log(myLibrary);
 });
 
+// temporary
 const book1 = new Book(
 	"12 Rules for Life",
 	"Jordan Peterson",
 	"489",
-	"have read"
+	"Have read"
 );
 
-const book2 = new Book("The Widow", "Fiona Barton", "324", "have read");
+const book2 = new Book("The Widow", "Fiona Barton", "324", "Have read");
 
-const book3 = new Book("Fahrenheit 451", "Rad Bradbury", "194", "not read yet");
+const book3 = new Book(
+	"Fahrenheit 451",
+	"Rad Bradbury",
+	"194",
+	"Have not read yet"
+);
 
 const book4 = new Book(
 	"The Gulag Archipelago",
 	"Aleksandr Solzhenitsyn",
 	"472",
-	"not read yet"
+	"Have not read yet"
 );
 
-const book5 = new Book("The Republic", "Plato", "416", "not read yet");
+const book5 = new Book("The Republic", "Plato", "416", "Have not read yet");
 const book6 = new Book(
 	"Dunia Tanpa Tembok ",
 	"Ayman Rashdan Wong",
 	"245",
-	"not read yet"
+	"Have not read yet"
 );
 
 book1.addBookToLibrary(book1);
